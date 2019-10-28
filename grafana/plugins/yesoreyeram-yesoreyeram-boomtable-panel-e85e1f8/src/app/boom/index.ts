98e3e8d32d704b622734a8e16772aa7d9fdff567
Marty S <martysohio@gmail.com>
2019-10-28 17:58:40 -0400

    docker stack

--- grafana/plugins/yesoreyeram-yesoreyeram-boomtable-panel-e85e1f8/src/app/boom/index.ts
+++
@@ -1,5 +0,0 @@
-export { IBoomPattern, IBoomSeries, IBoomTimeBasedThreshold, IBoomRenderingOptions, IBoomTable, IBoomHTML, IBoomCellDetails } from "./Boom.interface";
-export { normalizeColor, replaceTokens, getActualNameWithoutTokens, getDecimalsForValue, getItemBasedOnThreshold } from "./BoomUtils";
-export { BoomTimeBasedThreshold } from "./BoomTimeBasedThreshold";
-export { BoomPattern } from "./BoomPattern";
-export { BoomSeries } from "./BoomSeries";
